89962806d1aaa343dda2f31259821431e9e30d20
Tony Quetano <tquetano@netflix.com>
2020-04-26 16:05:59 -0700

    add cleanup to each test suite

--- src/bar.spec.ts
+++ src/bar.spec.ts
@@ -1,6 +1,13 @@
 import { add } from './bar';
 
 describe('Bar', () => {
+    afterEach(() => {
+        add.expirations.forEach((expiration) =>
+            clearTimeout(expiration.timeoutId)
+        );
+        add.expirations.length = 0;
+    });
+
     describe('When adding two numbers', () => {
         it('it adds the numbers', async () => {
             await expect(add(3, 2)).resolves.toEqual(5);
